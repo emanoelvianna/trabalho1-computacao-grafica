737277a3b9c80acb0a0a018ecb2a75ad058f88f5
Gabriell Araujo <ZK9RX6@users.noreply.github.com>
2017-05-01 20:02:34 -0300

    Imprime na tela qual marcador está sendo capturado

--- src/main_source.c
+++ src/main_source.c
@@ -16,64 +16,70 @@
 #include <AR/param.h>
 #include <AR/ar.h>
 #include <AR/arMulti.h>
+#include <AR/config.h>
 
 /* set up the video format globals */
 
 #ifdef _WIN32
-char			*vconf = "Data\\WDM_camera_flipV.xml";
+char *vconf = "Data\\WDM_camera_flipV.xml";
 #else
-char			*vconf = "";
+char *vconf = "";
 #endif
 
-int             xsize, ysize;
-int             thresh = 100;
-int             count = 0;
+int xsize, ysize;
+int thresh = 100;
+int count = 0;
 
-char           *cparam_name    = "Data/camera_para.dat";
-ARParam         cparam;
+char *cparam_name = "Data/camera_para.dat";
+ARParam cparam;
 
-char                *config_name = "Data/multi/marker.dat";
-ARMultiMarkerInfoT  *config;
+char *config_name = "Data/multi/marker.dat";
+ARMultiMarkerInfoT *config;
 
-static void   init(void);
-static void   cleanup(void);
-static void   keyEvent( unsigned char key, int x, int y);
-static void   mainLoop(void);
-static void   draw( double trans1[3][4], double trans2[3][4], int mode );
+static void init(void);
+static void cleanup(void);
+static void keyEvent( unsigned char key, int x, int y);
+static void mainLoop(void);
+static void draw( double trans1[3][4], double trans2[3][4], int mode );
 
 
 int main(int argc, char **argv)
 {
-	glutInit(&argc, argv);
+    glutInit(&argc, argv);
     init();
 
     arVideoCapStart();
     argMainLoop( NULL, keyEvent, mainLoop );
-	return (0);
+    return (0);
 }
 
 static void   keyEvent( unsigned char key, int x, int y)
 {
     /* quit if the ESC key is pressed */
-    if( key == 0x1b ) {
+    if( key == 0x1b )
+    {
         printf("*** %f (frame/sec)\n", (double)count/arUtilTimer());
         cleanup();
         exit(0);
     }
 
-    if( key == 't' ) {
+    if( key == 't' )
+    {
         printf("*** %f (frame/sec)\n", (double)count/arUtilTimer());
         printf("Enter new threshold value (current = %d): ", thresh);
-        scanf("%d",&thresh); while( getchar()!='\n' );
+        scanf("%d",&thresh);
+        while( getchar()!='\n' );
         printf("\n");
         count = 0;
     }
 
     /* turn on and off the debug mode with right mouse */
-    if( key == 'd' ) {
+    if( key == 'd' )
+    {
         printf("*** %f (frame/sec)\n", (double)count/arUtilTimer());
         arDebug = 1 - arDebug;
-        if( arDebug == 0 ) {
+        if( arDebug == 0 )
+        {
             glClearColor( 0.0, 0.0, 0.0, 0.0 );
             glClear(GL_COLOR_BUFFER_BIT);
             argSwapBuffers();
@@ -95,7 +101,8 @@ static void mainLoop(void)
     int             i;
 
     /* grab a vide frame */
-    if( (dataPtr = (ARUint8 *)arVideoGetImage()) == NULL ) {
+    if( (dataPtr = (ARUint8 *)arVideoGetImage()) == NULL )
+    {
         arUtilSleep(2);
         return;
     }
@@ -103,25 +110,43 @@ static void mainLoop(void)
     count++;
 
     /* detect the markers in the video frame */
-    if( arDetectMarkerLite(dataPtr, thresh, &marker_info, &marker_num) < 0 ) {
+    if( arDetectMarkerLite(dataPtr, thresh, &marker_info, &marker_num) < 0 )
+    {
         cleanup();
         exit(0);
     }
 
+	//VERIFICA MARCADOR CAPTURADO
+    char marcadores_char[6];
+	marcadores_char[0] = 'a';
+	marcadores_char[1] = 'b';
+	marcadores_char[2] = 'c';
+	marcadores_char[3] = 'd';
+	marcadores_char[4] = 'g';
+	marcadores_char[5] = 'f';
+	printf("MARCADOR_CAPTURADO=%c\n",marcadores_char[marker_info->id]);
+
     argDrawMode2D();
-    if( !arDebug ) {
+    if( !arDebug )
+    {
         argDispImage( dataPtr, 0,0 );
     }
-    else {
+    else
+    {
         argDispImage( dataPtr, 1, 1 );
         if( arImageProcMode == AR_IMAGE_PROC_IN_HALF )
+        {
             argDispHalfImage( arImage, 0, 0 );
+        }
         else
+        {
             argDispImage( arImage, 0, 0);
+        }
 
         glColor3f( 1.0, 0.0, 0.0 );
         glLineWidth( 1.0 );
-        for( i = 0; i < marker_num; i++ ) {
+        for( i = 0; i < marker_num; i++ )
+        {
             argDrawSquare( marker_info[i].vertex, 0, 0 );
         }
         glLineWidth( 1.0 );
@@ -129,27 +154,24 @@ static void mainLoop(void)
 
     arVideoCapNext();
 
-    if( (err=arMultiGetTransMat(marker_info, marker_num, config)) < 0 ) {
+    if( (err=arMultiGetTransMat(marker_info, marker_num, config)) < 0 )
+    {
         argSwapBuffers();
         return;
     }
-    printf("err = %f\n", err);
-    if(err > 100.0 ) {
+	//printf("err = %f\n", err);
+    if(err > 100.0 )
+    {
         argSwapBuffers();
         return;
     }
-/*
-    for(i=0;i<3;i++) {
-        for(j=0;j<4;j++) printf("%10.5f ", config->trans[i][j]);
-        printf("\n");
-    }
-    printf("\n");
-*/
+
     argDrawMode3D();
     argDraw3dCamera( 0, 0 );
     glClearDepth( 1.0 );
     glClear(GL_DEPTH_BUFFER_BIT);
-    for( i = 0; i < config->marker_num; i++ ) {
+    for( i = 0; i < config->marker_num; i++ )
+    {
         if( config->marker[i].visible >= 0 ) draw( config->trans, config->marker[i].trans, 0 );
         else                                 draw( config->trans, config->marker[i].trans, 1 );
     }
@@ -167,7 +189,8 @@ static void init( void )
     printf("Image size (x,y) = (%d,%d)\n", xsize, ysize);
 
     /* set the initial camera parameters */
-    if( arParamLoad(cparam_name, 1, &wparam) < 0 ) {
+    if( arParamLoad(cparam_name, 1, &wparam) < 0 )
+    {
         printf("Camera parameter load error !!\n");
         exit(0);
     }
@@ -176,7 +199,8 @@ static void init( void )
     printf("*** Camera Parameter ***\n");
     arParamDisp( &cparam );
 
-    if( (config = arMultiReadConfigFile(config_name)) == NULL ) {
+    if( (config = arMultiReadConfigFile(config_name)) == NULL )
+    {
         printf("config data load error !!\n");
         exit(0);
     }
@@ -222,7 +246,8 @@ static void draw( double trans1[3][4], double trans2[3][4], int mode )
     argConvGlpara(trans2, gl_para);
     glMultMatrixd( gl_para );
 
-    if( mode == 0 ) {
+    if( mode == 0 )
+    {
         glEnable(GL_LIGHTING);
         glEnable(GL_LIGHT0);
         glLightfv(GL_LIGHT0, GL_POSITION, light_position);
@@ -232,7 +257,8 @@ static void draw( double trans1[3][4], double trans2[3][4], int mode )
         glMaterialfv(GL_FRONT, GL_SHININESS, mat_flash_shiny);
         glMaterialfv(GL_FRONT, GL_AMBIENT, mat_ambient);
     }
-    else {
+    else
+    {
         glEnable(GL_LIGHTING);
         glEnable(GL_LIGHT0);
         glLightfv(GL_LIGHT0, GL_POSITION, light_position);
@@ -245,7 +271,7 @@ static void draw( double trans1[3][4], double trans2[3][4], int mode )
     glMatrixMode(GL_MODELVIEW);
     glTranslatef( 0.0, 0.0, 25.0 );
     if( !arDebug ) glutSolidCube(50.0);
-     else          glutWireCube(50.0);
+    else          glutWireCube(50.0);
     glDisable( GL_LIGHTING );
 
     glDisable( GL_DEPTH_TEST );
